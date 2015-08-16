241bbd196bd6dc3ece7aa4bb564126bf4e1d1f45
Diego Medina <d.medina1290@gmail.com>
2015-08-16 15:46:08 -0400

    Create db

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RainCheck::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   devise_for :users
-  root to: "home#index"
+  root to: "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
